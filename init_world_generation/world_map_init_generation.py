9830592a44cc580dc574b9a86f48d11fcc7cac6d
moroha-hunter <80098980+moroha-hunter@users.noreply.github.com>
2021-06-24 14:31:18 +0000

    Add files via upload

--- init_world_generation/world_map_init_generation.py
+++ init_world_generation/world_map_init_generation.py
@@ -4,7 +4,11 @@ def init_zone_sequence_generation(first_world_model_zones_total_amount = 100, zo
  first_world_model_zone_sequence = random.choice(zone_types, size=(first_world_model_zones_total_amount)) #zones random generation
  first_world_model_zone_sequence[0] = the_central_zone_id #replace the first element in array with the central zone
  return first_world_model_zone_sequence #return the array with zones sequence
-def init_zone_sizes_generation(first_world_model_zones_total_amount = 100, the_central_zone_range = 100, min_zone_range = 20, max_zone_range = 60):
- first_world_model_zone_sizes = random.randint(min_zone_range,max_zone_range,size=first_world_model_zones_total_amount)
- first_world_model_zone_sizes[0] = the_central_zone_range
- return first_world_model_zone_sizes
+def init_zone_ranges_generation(first_world_model_zones_total_amount = 100, the_central_zone_range = 100, min_zone_range = 20, max_zone_range = 60):
+ first_world_model_zone_ranges = random.randint(min_zone_range,max_zone_range,size=first_world_model_zones_total_amount)
+ first_world_model_zone_ranges[0] = the_central_zone_range
+ for counter in range(len(first_world_model_zone_ranges)):
+  if counter == 0:
+   continue
+  first_world_model_zone_ranges[counter] = first_world_model_zone_ranges[counter - 1] + first_world_model_zone_ranges[counter]
+ return first_world_model_zone_ranges
