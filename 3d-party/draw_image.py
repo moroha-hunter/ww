84e0c3b6bea68821cc5cee02a8ccbf8adcfa8216
moroha-hunter <80098980+moroha-hunter@users.noreply.github.com>
2021-06-26 10:39:41 +0000

    Update draw_image.py

--- 3d-party/draw_image.py
+++ 3d-party/draw_image.py
@@ -5,7 +5,7 @@ filename = '/tmp/perlin4000x4000.binary' #float number on each line, these numbe
 f = open(filename, 'r')
 
 result = []
-for i in range(4000):
+for i in range(4000): #Make one common list with 4000 lists inside, each list contains 4000 float numbers
     tmp = []
     for j in range(4000):
         tmp.append(float(f.readline()[:-1]))
